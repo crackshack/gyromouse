0f5912b4e0c2029ebf4defcc42c6e33d9542e17d
Martin Milchevski <75259519+Macka323@users.noreply.github.com>
2025-05-11 10:50:16 +0200

    Dodavanje Martin verzijata
    ne e stabilno i samo za testiranje

---
+++ GetStartedApp/GetStartedApp/Views/MainView.axaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Notification;
+using Avalonia.Threading;
+using Serilog;
+using WindowsInput;
+using WindowsInput.Native;
+
+
+namespace GetStartedApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+        Log.Information("Application started");
+    }
+    SerialPort serialPort = new SerialPort();
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(Celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            Fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
+        DropDown.Items.Clear();
+    }
+
+    private async void Refresh_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        DropDown.Items.Clear();
+        //var serport = new SerialPort(DropDown.SelectedItem as string);
+        foreach (var item in SerialPort.GetPortNames())
+        {
+            DropDown.Items.Add(item);
+        }
+
+
+        return;
+    }
+
+    private void Connect_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (serialPort is null)
+            serialPort = new SerialPort();
+        if (serialPort.IsOpen)
+            return;
+
+        serialPort.PortName = DropDown.SelectedItem as string;
+        serialPort.BaudRate = 115200;
+        serialPort.DataReceived += SerialPort_DataReceived;
+
+
+        serialPort.Open();
+    }
+
+
+    bool processing = false;
+    InputSimulator simulator = new InputSimulator();
+    int numberOfSamples = 0;
+    float[] samplesX = new float[1000], samplesY = new float[1000];
+
+    private async void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        var port = sender as SerialPort;
+
+
+        if (!processing)
+        {
+            processing = true;
+
+
+            await Dispatcher.UIThread.InvokeAsync(async () =>
+            {
+                if (port is null)
+                    return;
+                if (!port.IsOpen)
+                    return;
+
+                var pomosna = port.ReadLine();
+
+                float firstVariable = 0, secondVariable = 0;
+                var text = pomosna.Split(',');
+                if (text.Length >= 5)
+                {
+                    float.TryParse(text[3], out firstVariable);
+
+                    float.TryParse(text[5], out secondVariable);
+                }
+                //za kalibracija na offset
+                #region Calibration
+                /*if (numberOfSamples < 500)
+                {
+                    samplesX[numberOfSamples] = x;
+                    samplesY[numberOfSamples] = y;
+                    numberOfSamples++;
+                }
+                else
+                    SerialOut.Text = $"avgX {(samplesX.Sum() / 500)}  avgY {(samplesY.Sum() / 500)}";
+                */
+                firstVariable = firstVariable + 3.052f;
+                secondVariable = secondVariable + 1.11f;
+                #endregion
+
+                #region Deadzone
+                if (Math.Abs(firstVariable) < 2)
+                    firstVariable = 0;
+                if (Math.Abs(secondVariable) < 2)
+                    secondVariable = 0;
+                #endregion
+
+                #region speed
+                firstVariable /= 1.8f;
+                secondVariable /= 1.8f;
+                #endregion
+                simulator.Mouse.MoveMouseBy((int)-secondVariable, (int)firstVariable);
+
+                //    SerialOut.Text = $"{-(int)(y / 5 +0.4)}  {(int)(x/5 +1.15)}";
+            });
+
+
+            processing = false;
+        }
+        //flush old data
+        if (port is null)
+            return;
+        if (!port.IsOpen)
+            return;
+        if (port.BytesToRead > 50)
+            port.ReadLine();
+
+
+    }
+
+    private void Discconect_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (serialPort is not null)
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+    }
+
+    private async void Button_Click_1(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+       
+    }
+  
+}
---
+++ ble mouse/src/main.cpp
@@ -0,0 +1,134 @@
+/*
+https://lastminuteengineers.com/mpu6050-accel-gyro-arduino-tutorial/
+
+bibliotekata za senzoro i nacino na komunikacija sa tolku mn testirani i probavani sho mi e omrznal zivoto
+*/
+
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+#include <Wire.h>
+#include "GY521.h"
+#define DEBUG 0
+#include <esp_now.h>
+#include <WiFi.h>
+GY521 sensor(0x68);
+
+// 08:3a:f2:52:28:64
+//  REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0x08, 0x3a, 0xf2, 0x52, 0x28, 0x64};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message
+{
+  float ax;
+  float ay;
+  float az;
+  float gx;
+  float gy;
+  float gz;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void setup()
+{
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  while (sensor.wakeup() == false)
+  {
+    Serial.print(millis());
+    Serial.println("\tCould not connect to GY521: please check the GY521 address (0x68/0x69)");
+    delay(1000);
+  }
+  Serial.println("skiping connecting to gy521 oti testiram ble");
+  sensor.setAccelSensitivity(0); //  2g
+  sensor.setGyroSensitivity(0);  //  250 degrees/s
+
+  sensor.setThrottle();
+  Serial.println("start...");
+
+  //  set calibration values from calibration sketch.
+  sensor.axe = 0;
+  sensor.aye = 0;
+  sensor.aze = 0;
+  sensor.gxe = 0;
+  sensor.gye = 0;
+  sensor.gze = 0;
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+
+void loop()
+{
+  sensor.read();
+  float ax = sensor.getAccelX();
+  float ay = sensor.getAccelY();
+  float az = sensor.getAccelZ();
+  float gx = sensor.getGyroX();
+  float gy = sensor.getGyroY();
+  float gz = sensor.getGyroZ();
+  float t = sensor.getTemperature();
+
+  myData.ax = ax;
+  myData.ay = ay;
+  myData.az = az;
+  myData.gx = gx;
+  myData.gy = gy;
+  myData.gz = gz;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  delay(10);
+}
